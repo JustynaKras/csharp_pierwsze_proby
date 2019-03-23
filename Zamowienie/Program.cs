639a87bfc1f7f306d9943c551795d235f75a9f8d
Justyna Kras <justyna.kras@student.wat.edu.pl>
2019-03-23 22:10:14 +0100

    nie wiem jak uruchomić ten program

---
+++ Zamowienie/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zamowienie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = args[0];  //przejdz do tablicy args, daj mi pierwszy element i przypisz do zmiennej name
+
+            Console.WriteLine("Witaj świecie mój kochany, zakałdamy bazę klientów" + name);
+           
+        }
+    }
+}
